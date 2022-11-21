4880bcf7957f3bf627c7a04bb89d4279b3b733ee
bongchia <nhub1910120@student.ctu.edu.vn>
2022-11-21 19:53:46 +0700

    xong backend roi

--- backend/app/controllers/TaskController.js
+++ backend/app/controllers/TaskController.js
@@ -33,17 +33,81 @@ class TaskController {
       });
     }
   }
-  getTasks(req, res) {
-    res.send("lay tat ca bai viet");
+  async getTasks(req, res) {
+    const userId = req.userId;
+
+    try {
+      const tasks = await Task.find({ poster: userId });
+      res.status(201).json({
+        message: "Lay tasks thanh cong",
+        tasks,
+        success: true,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: "Server khong phan hoi",
+        success: false,
+      });
+    }
   }
   getTask(req, res) {
     res.send("lay tat ca bai viet");
   }
-  editTask(req, res) {
-    res.send("lay tat ca bai viet");
+  async editTask(req, res) {
+    const userId = req.userId;
+    const taskId = req.params.id;
+
+    const { title, body, status } = req.body;
+
+    if (title == "" || body == "") {
+      return res.status(403).json({
+        message: "Khong duoc bo trong cac truong",
+        success: false,
+      });
+    }
+
+    let updateTask = { title, body, status };
+
+    try {
+      updateTask = await Task.findOneAndUpdate(
+        {
+          _id: taskId,
+          poster: userId,
+        },
+        updateTask,
+        { new: true }
+      );
+      res.status(203).json({
+        message: "update thanh cong",
+        success: true,
+        updateTask,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: "Server khong phan hoi",
+        success: false,
+      });
+    }
   }
-  deleteTask(req, res) {
-    res.send("lay tat ca bai viet");
+  async deleteTask(req, res) {
+    const userId = req.userId;
+    const taskId = req.params.id;
+
+    try {
+      const deleteTask = await Task.findOneAndDelete({
+        _id: taskId,
+        poster: userId,
+      });
+      res.status(203).json({
+        message: "Xoa bai viet thanh cong",
+        success: true,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: "Server khong phan hoi",
+        success: false,
+      });
+    }
   }
 }
 module.exports = new TaskController();
